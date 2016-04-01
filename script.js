24c24ae773d46ef4952e43deb1fd7851497c0f65
Brian Nguyen <nguyen.brian70@gmail.com>
2016-04-01 12:16:45 +0000

    beautiful morning

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 window.onload = function() {
-    
+        var music = new Audio("beautifulmorning.mp3")
+        var higher = new Audio("canwegetmuchhigher.mp3")
+        var kanye = new Image();
+        kanye.src = 'ThemanyfacesofKanye.jpg';
       setTimeout(function() {
          $("body").css("animation","shake 0.82s cubic-bezier(.36,.07,.19,.97) both infinite")
          $("a").css("animation", "rainbow 8s infinite")
@@ -7,10 +10,22 @@ window.onload = function() {
  
  
     setTimeout(function() {
-        for (i = 0; i < 10; i++) {
-            alert('hi')
-        } 
-
-      }, 15000)
+        for (i = 0; i < 4; i++) {
+            var array =["I used to love Kanye.", "Straight from the soul Kanye.", "And now I look and look around and" ,"There's so many Kanyes!"]
+            alert(array[i])
+        }
+        $("body").css("background-image","url('ThemanyfacesofKanye.jpg')")
+        $("#main").html("KANYE")
+        $("#main").css("font-size", "150px")+
+        music.play();
+        setTimeout(function() {
+            higher.play();
+            higher.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+higher.play();
+        }, 20000)
+      }, 5000)
  
 }
