856b89f110fea62bff52918d30846980bac7f3af
Brian Nguyen <nguyen.brian70@gmail.com>
2016-04-01 01:48:38 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    
+      setTimeout(function() {
+         $("body").css("animation","shake 0.82s cubic-bezier(.36,.07,.19,.97) both infinite")
+         $("a").css("animation", "rainbow 8s infinite")
+      }, 10000)
+ 
+}
